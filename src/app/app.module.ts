68e922c779c9215c744aeadd60efa80ee0c0479f
Laura Alarcón <lxalarcon0@misena.edu.co>
2024-04-21 15:36:51 -0500

    home structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { UserComponent } from './components/user/user.component';
 import { FavoriteComponent } from './components/favorite/favorite.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { CategoryComponent } from './components/category/category.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { MenuComponent } from './components/home/menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { CategoryComponent } from './components/category/category.component';
     UserComponent,
     FavoriteComponent,
     CheckoutComponent,
-    CategoryComponent
+    CategoryComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
